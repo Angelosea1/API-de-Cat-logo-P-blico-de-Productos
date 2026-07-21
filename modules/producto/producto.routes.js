2fe54ce4b5d32d8970b88e27f77c7dde5ac1bebb
Angelosea1 <ejrnnmsmmg@gmail.com>
2026-07-20 22:21:11 -0600

    Creacion de token

--- modules/producto/producto.routes.js
+++ modules/producto/producto.routes.js
@@ -6,8 +6,12 @@ import {
   updateProducto,
   deleteProducto,
 } from './producto.controller.js';
+import AppToken from '../../middlewares/AppToken.js';
 
 const router = Router();
+
+// Todas las rutas de /api/v1/productos requieren app-token válido
+router.use(AppToken);
 
 router.get('/', getProductos);
 router.get('/:id', getProductoById);
